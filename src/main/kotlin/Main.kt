be0f0a69b70810cf6222419583b6468fbbf6343e
Pavel Savvateev <savvateev@icloud.com>
2024-08-14 20:09:40 +0800

    new fun added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,34 @@ package ru.netolohy
 
 fun main() {
 
+    fun minRules(time : Int) : String {
+        return when ((time/60).toInt()) {
+                           1, 21, 31, 41, 51 -> "минуту назад"
+            2, 22, 32, 42, 52, 3, 23, 33, 43, 53, 4, 24, 34, 44, 54 -> "минуты назад"
+            else -> "минут назад"
+        }
+    }
+
+    fun hoursRules(time : Int) : String {
+        return when (time) {
+            1, 21 -> "час назад"
+            2, 3, 4, 22, 23, 24 -> "часа назад"
+            else -> "часов назад"
+        }
+    }
+
     fun agoToText(time : Int) : String {
         return when (time) {
             in 0..60 -> "был(а) только что"
-            in 61..3600 -> "был(а) ${time/60} минут назад"
-            in 3601..86400 -> "был(а) ${time/3600} часов назад"
+            in 61..3600 -> "был(а) ${time/60} ${minRules(time)}"
+            in 3601..86400 -> "был(а) ${time/3600} ${hoursRules(time/3600)}"
             in 86401..86400*2 -> "был(а) вчера"
             in (86400*2) +1..86400*3 -> "был(а) позавчера"
             else -> "был(а) давно"
         }
     }
     println(agoToText(30))
-    println(agoToText(80))
+    println(agoToText(3000))
     println(agoToText(10000))
     println(agoToText(100000))
     println(agoToText(200000))
